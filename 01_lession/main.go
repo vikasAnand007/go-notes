1b38f68994043be5051592dd09b07bea4b0576f5
vikasAnand007 <vikas7dev@gmail.com>
2025-02-27 07:41:14 +0530

    initial commit

---
+++ 01_lession/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt" // imported from go standard library
+
+// main function is the entry point in any go program
+func main() {
+	fmt.Println("Hello world!")
+}
+
+// compile
+// go build <filename>
+// this will generate a binary executable
+
+// OR
+// go run <filename>
+// automatically compiles and runs the code.
